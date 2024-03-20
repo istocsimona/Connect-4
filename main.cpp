890107cdc43568088e91ddb5fb872d6c2ad4d25a
istocsimona <istocsimona23@gmail.com>
2024-03-20 09:44:04 +0200

    Reparat greseli

--- main.cpp
+++ main.cpp
@@ -127,7 +127,7 @@ public:
             else nr=0;
         if(nr>=3) return 1;
 
-        //pe diagonala in sensul /
+        //pe diagonala in sensul / in jos
         nr=0;
         int egale=1;
         int i=lin, j=col;
@@ -141,9 +141,24 @@ public:
             }
             else egale=0;
         }
+        //pe diagonala in sensul / in sus
+        nr=0;
+        egale=1;
+        i=lin;
+        j=col;
+        while(egale==1)
+        {
+            if(tabla[lin][col]==tabla[i-1][j+1])
+            {
+                nr++;
+                i=i-1;
+                j=j+1;
+            }
+            else egale=0;
+        }
         if(nr>=3) return 1; //castigator
 
-        //pe diagonala in sensul \.
+        //pe diagonala in sensul \ in jos
         nr=0;
         egale=1;
         i=lin;
@@ -161,6 +176,21 @@ public:
         if(nr>=3)
             return 1;//castigator
 
+        //pe diagonala in sensul  \ in sus
+        nr=0;
+        egale=1;
+        i=lin;
+        j=col;
+        while(egale==1)
+        {
+            if(tabla[lin][col]==tabla[i-1][j-1])
+            {
+                nr++;
+                i=i-1;
+                j=j-1;
+            }
+            else egale=0;
+        }
         return 0;//necastigator
     }
     void afisare()
@@ -171,6 +201,11 @@ public:
                 cout<<tabla[i][j]<<" ";
             cout<<endl;
         }
+    }
+    int coloanafull(int col)
+    {
+        if(tabla[1][col]!='^') return 0;//nu avem piesa pana sus
+        return 1;//coloana e full
     }
     int gravitatie(int col)
     {//elementul se va pune pe linia 1 evident si coloana col, stiind asta o coboram pana jos sau pana la ultima piesa daca e
@@ -220,23 +255,42 @@ int main() {
         if(tura_jucator.getMutare()==1)
         {
             cout<<"Tura lui "<<jucator1.getNume()<<endl;
+
             cout<<"Indroduceti numarul coloanei dorite: ";
             operator>>(std::cin, coloana_coresp);
+            while(coloana_coresp.getPiesa()-1<0 || coloana_coresp.getPiesa()-1>6 || tabla_joc.coloanafull(coloana_coresp.getPiesa()-1)==0)
+            {//verificam daca coloana exista
+                cout<<"Coloana inexistenta sau plina"<<endl;
+                cout<<"Indroduceti numarul coloanei dorite: ";
+                operator>>(std::cin, coloana_coresp);
+            }
             cout<<endl;
-            tabla_joc.setElement(coloana_coresp.getPiesa()-1, 'o');
-            int t=tabla_joc.gravitatie(coloana_coresp.getPiesa());
+
+            tabla_joc.setElement(coloana_coresp.getPiesa()-1, 'o');//punem elememtul pe coloana ceruta
+
+            int t=tabla_joc.gravitatie(coloana_coresp.getPiesa()); //activam gravitatia pentru piesa pusa si in t pastram linia la care e pentru mai tarziu
+
             if(tabla_joc.verificare_jucator(t, coloana_coresp.getPiesa()-1))
-                castigare.setCastigare(1);
+                castigare.setCastigare(1);//verificam daca piesa a facut o miscare castigatoare
+
             tabla_joc.afisare();
-            tura_jucator.setMutare(2);
+            tura_jucator.setMutare(2);//mutam tura la jucatorul 2
+
             if(castigare.getCastigare()==1)
                 cout<<"Felicitari "<<jucator1.getNume()<<"!!!!!";
         }
         else
         {
             cout<<"Tura lui "<<jucator2.getNume()<<endl;
+
             cout<<"Indroduceti numarul coloanei dorite: ";
             operator>>(std::cin, coloana_coresp);
+            while(coloana_coresp.getPiesa()-1<0 || coloana_coresp.getPiesa()-1>6 || tabla_joc.coloanafull(coloana_coresp.getPiesa()-1)==0)
+            {//verificam daca coloana exista
+                cout<<"Coloana inexistenta sau plina"<<endl;
+                cout<<"Indroduceti numarul coloanei dorite: ";
+                operator>>(std::cin, coloana_coresp);
+            }
             cout<<endl;
             tabla_joc.setElement(coloana_coresp.getPiesa()-1, 'x');
             int t=tabla_joc.gravitatie(coloana_coresp.getPiesa());
